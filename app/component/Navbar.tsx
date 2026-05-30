596264391ee1567d1a6c1b67eeaffd404792014a
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-05-30 07:38:16 +0100

    commit

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   return (
     <header className="w-full bg-white shadow-xs fixed top-0 left-0 z-50 p-2">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           {/* Logo */}
           <div className="logo">

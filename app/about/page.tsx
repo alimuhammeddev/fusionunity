ce054ef21103ed2d94843f8cf5f772846e2db93c
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-05-31 11:09:11 +0100

    commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../component/Navbar"
+
+export default function About() {
+    return (
+        <section>
+            <Navbar />
+            <div>
+                About Us
+            </div>
+        </section>
+    )
+}
